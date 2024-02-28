ce859d68750740d1fdec9fec482fbc6c1155290a
Oleksii_Baiev <bayevaleksey@gmail.com>
2024-02-28 13:38:34 +0200

    Change Route in App

--- src/App.jsx
+++ src/App.jsx
@@ -55,8 +55,8 @@ function App() {
                   }
                 />
               </Route>
-              <Route path="*" element={<NotFoundPage />} />
             </Routes>
+						<Route path="*" element={<NotFoundPage />} />
           </SelectedBooksProvider>
         </BooksProvider>
       </UserProvider>
