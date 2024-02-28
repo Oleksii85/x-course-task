9d82c3a3a7e2695564f810d2ee920db903ade096
Oleksii_Baiev <bayevaleksey@gmail.com>
2024-02-28 21:24:23 +0200

    update App & index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React, { StrictMode } from "react";
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import './index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </StrictMode>
 );
