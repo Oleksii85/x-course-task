93825f03d7ea5fa97594dc4236cccc23276601f1
Oleksii_Baiev <bayevaleksey@gmail.com>
2024-03-01 11:21:00 +0200

    change HashRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
@@ -7,8 +7,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/x-course-task">
+    <HashRouter basename="/x-course-task">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
